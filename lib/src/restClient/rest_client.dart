9ca9cb228a0bdf5549069fcfc4e5f4946a098d93
JoaoRafa19 <joaopedrorafael19@gmail.com>
2024-05-21 14:26:02 -0300

    fix: Rest client para web

--- lib/src/restClient/rest_client.dart
+++ lib/src/restClient/rest_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:dio/browser.dart';
 import 'package:dio/io.dart';
 import 'package:fe_lab_clinicas_core/src/restClient/interceptors/auth_interceptor.dart';
 
@@ -25,6 +26,35 @@ final class RestClient extends DioForNative {
   }
 
   RestClient get unAuth {
+    options.extra['DIO_AUTH_KEY'] = false;
+    return this;
+  }
+}
+
+final class RestClientWeb extends DioForBrowser {
+  RestClientWeb(String baseUrl)
+      : super(
+          BaseOptions(
+            baseUrl: baseUrl,
+            connectTimeout: const Duration(seconds: 10),
+            receiveTimeout: const Duration(seconds: 60),
+          ),
+        ) {
+    interceptors.addAll([
+      LogInterceptor(
+        requestBody: true,
+        responseBody: true,
+      ),
+      AuthInterceptor(),
+    ]);
+  }
+
+  RestClientWeb get auth {
+    options.extra['DIO_AUTH_KEY'] = true;
+    return this;
+  }
+
+  RestClientWeb get unauth {
     options.extra['DIO_AUTH_KEY'] = false;
     return this;
   }
